b1e0992aaa1507b767903e824963834cb7b23982
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-06-27 19:38:48 +0200

    feat: add conditional rendering with v-if and v-else

--- 1-intro/src/App.vue
+++ 1-intro/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>Hello {{ name }}</h1>
+
+  <p v-if="professor">User is a professor</p>
+  <p v-else>User is not a professor</p>
 </template>
 
 <script>
@@ -7,7 +10,8 @@
     name: "App",
     data() {
       return {
-        name: "Vica"
+        name: "Vica",
+        professor: false
       }
     }
   }
