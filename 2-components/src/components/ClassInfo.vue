d0f58a74a8343bd530d47d1344911fc10313e15e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-06-29 11:07:19 +0200

    feat: add ClassInfo component

---
+++ 2-components/src/components/ClassInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Course: {{ className }}</p>
+</template>
+
+<script>
+export default {
+  name: "ClassInfoComponent",
+  props: {
+    className: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      courseInfo: {
+        "ReactJS": {
+          price: 350,
+          length: "3 months",
+          numberOfLectures: 24,
+        }
+      }
+
+
+    }
+  }
+}
+</script>
