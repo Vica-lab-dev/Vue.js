7cc48de885c245ca6ad56053e8c99e5f033b90e9
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-02 10:12:05 +0200

    Import Products component into App.vue

---
+++ 3-hooks-and-axios/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  Hello world from main!
+  <Products></Products>
+</template>
+
+<script>
+import Products from "@/components/Products.vue";
+
+export default {
+  name: "App",
+  components: {Products},
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
