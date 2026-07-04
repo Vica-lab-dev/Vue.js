14f18cd3478e2b83a74d06d3b16b15080e794069
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-04 04:48:31 +0200

    refactor: move router-view below navigation links

--- 3-hooks-and-axios/src/App.vue
+++ 3-hooks-and-axios/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <router-view></router-view>
   <router-link :to="{name: 'Home'}">Homepage</router-link>
   <router-link :to="{name: 'Products'}">Products</router-link>
+  <router-view></router-view>
 </template>
 
 <script>
