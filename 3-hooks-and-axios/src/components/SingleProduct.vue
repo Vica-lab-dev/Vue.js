309fe1a463472869970b77056657c23756a8538d
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-04 05:35:44 +0200

    * Homework 4

---
+++ 3-hooks-and-axios/src/components/SingleProduct.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{ this.product.id }}</p>
+  <p>{{ this.product.description }}</p>
+  <p>${{ this.product.price }}</p>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      product: {}
+    }
+  },
+
+  mounted() {
+    axios.get(process.env.VUE_APP_DUMMYJSON_API+"/products/"+this.id)
+        .then(response => this.product = response.data)
+        .finally(() => console.log(this.product))
+  }
+}
+</script>
--- 3-hooks-and-axios/src/views/SingleProductView.vue
+++ 3-hooks-and-axios/src/views/SingleProductView.vue
@@ -1,13 +1,24 @@
 <template>
   {{ productId }}
+  <SingleProduct :id="productId"></SingleProduct>
 </template>
 
 <script>
+import SingleProduct from "@/components/SingleProduct.vue";
+
 export default {
+  components: {SingleProduct},
   computed: {
     productId() {
       return this.$route.params.id;
     }
+  },
+
+  beforeCreate() {
+    if(isNaN(this.$route.params.id) || this.$route.params.id < 1) {
+      this.$router.replace({name: "Products"});
+    }
   }
+
 }
 </script>
