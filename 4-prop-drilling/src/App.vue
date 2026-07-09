051aed840606034fac8c6496951ae0e2639fbb82
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-09 09:07:53 +0200

    feat: use provide/inject for product list

--- 4-prop-drilling/src/App.vue
+++ 4-prop-drilling/src/App.vue
@@ -1,10 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <ProductsPage></ProductsPage>
 </template>
 
 <script>
 
+import ProductsPage from "@/components/ProductsPage.vue";
+
 export default {
   name: 'App',
+  components: {ProductsPage},
+  provide () {
+    return {
+      productList: this.productList
+    }
+  },
+  data: () => ({
+    productList: [
+      {
+        id: 1,
+        name: "HTML Course",
+        price: 150,
+        length: "3 months"
+      },
+      {
+        id: 2,
+        name: "CSS Masterclass",
+        price: 180,
+        length: "4 months"
+      },
+      {
+        id: 3,
+        name: "JavaScript Bootcamp",
+        price: 250,
+        length: "6 months"
+      },
+      {
+        id: 4,
+        name: "Vue.js Fundamentals",
+        price: 220,
+        length: "5 months"
+      },
+      {
+        id: 5,
+        name: "Node.js Backend Development",
+        price: 300,
+        length: "7 months"
+      }
+    ]
+  }),
+
 }
 </script>
---
+++ 4-prop-drilling/src/components/ProductDetails.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Hello world form details</p>
+</template>
+<script>
+export default {
+  name: "ProductDetails",
+  inject: ["productList"],
+  mounted() {
+    console.log(this.productList);
+  }
+}
+</script>
