e200a0bd4da1a5202b4753f0494eae57ebccba54
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-09 09:11:35 +0200

    feat: display product list using v-for

--- 4-prop-drilling/src/components/ProductDetails.vue
+++ 4-prop-drilling/src/components/ProductDetails.vue
@@ -1,12 +1,14 @@
 <template>
   <p>Hello world form details</p>
+  <ul>
+    <li v-for="product in productList" :key="product.id">
+      {{ product.name }} - ${{ product.price }}
+    </li>
+  </ul>
 </template>
 <script>
 export default {
   name: "ProductDetails",
   inject: ["productList"],
-  mounted() {
-    console.log(this.productList);
-  }
 }
 </script>
