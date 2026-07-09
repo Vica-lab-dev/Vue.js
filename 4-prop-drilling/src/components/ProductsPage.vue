ea2b1594a466d877c4e5f2fc1f3e44ff2e8d9271
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-09 09:07:06 +0200

    feat: add ProductDetails component to ProductsPage

---
+++ 4-prop-drilling/src/components/ProductsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Hello World</p>
+  <ProductDetails></ProductDetails>
+</template>
+
+<script>
+import ProductDetails from "@/components/ProductDetails.vue";
+
+export default {
+  name: 'ProductsPage',
+  components: {ProductDetails},
+}
+</script>
