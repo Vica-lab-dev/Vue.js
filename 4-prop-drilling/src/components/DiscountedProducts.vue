9a46ef0279bb8bb523201dc50d813bd4b733ba64
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-09 09:26:43 +0200

    feat: filter and display discounted products

---
+++ 4-prop-drilling/src/components/DiscountedProducts.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Discounted Products</h1>
+  <ul>
+    <li v-for="product in discountedProducts" :key="product.id">
+      {{ product.name }} - ${{ product.price }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "DiscountedProducts",
+  inject: ['productList'],
+  computed: {
+    discountedProducts() {
+      return this.productList.filter(product => product.discount !== null)
+    }
+  }
+}
+</script>
--- 4-prop-drilling/src/components/ProductsPage.vue
+++ 4-prop-drilling/src/components/ProductsPage.vue
@@ -1,13 +1,14 @@
 <template>
-  <p>Hello World</p>
+ <DiscountedProducts></DiscountedProducts>
   <ProductDetails></ProductDetails>
 </template>
 
 <script>
 import ProductDetails from "@/components/ProductDetails.vue";
+import DiscountedProducts from "@/components/DiscountedProducts.vue";
 
 export default {
   name: 'ProductsPage',
-  components: {ProductDetails},
+  components: {DiscountedProducts, ProductDetails},
 }
 </script>
