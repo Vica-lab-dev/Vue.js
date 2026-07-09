467836e18e23c73ebe42d2df5af6e58c8bf3923e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-09 08:45:38 +0200

    refactor: remove Users and Info components

--- 4-prop-drilling/src/App.vue
+++ 4-prop-drilling/src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Users msg="Hello prop" :currentDate="currentDate"></Users>
 </template>
 
 <script>
-import Users from './components/Users.vue'
 
 export default {
   name: 'App',
-  data: () => ({
-    currentDate: "09/07/2026"
-  }),
-  components: {
-    Users
-  }
 }
 </script>
