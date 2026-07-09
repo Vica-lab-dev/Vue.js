a0be0600b7706f04b7797a5cad8e958b011426eb
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-09 09:23:55 +0200

    feat: add discount field to products

--- 4-prop-drilling/src/App.vue
+++ 4-prop-drilling/src/App.vue
@@ -20,31 +20,36 @@ export default {
         id: 1,
         name: "HTML Course",
         price: 150,
-        length: "3 months"
+        length: "3 months",
+        discount: "50%"
       },
       {
         id: 2,
         name: "CSS Masterclass",
         price: 180,
-        length: "4 months"
+        length: "4 months",
+        discount: null
       },
       {
         id: 3,
         name: "JavaScript Bootcamp",
         price: 250,
-        length: "6 months"
+        length: "6 months",
+        discount: "50%"
       },
       {
         id: 4,
         name: "Vue.js Fundamentals",
         price: 220,
-        length: "5 months"
+        length: "5 months",
+        discount: null
       },
       {
         id: 5,
         name: "Node.js Backend Development",
         price: 300,
-        length: "7 months"
+        length: "7 months",
+        discount: null
       }
     ]
   }),
