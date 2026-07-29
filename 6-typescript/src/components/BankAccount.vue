1dd62a9cfa70cac9751fb9bb11d69dd9a8b89c5a
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-29 11:27:24 +0200

    chore: display account notes using v-for

--- 6-typescript/src/components/BankAccount.vue
+++ 6-typescript/src/components/BankAccount.vue
@@ -2,6 +2,9 @@
   <p>Owner: {{ name }}</p>
   <p>Money: {{ amount }}</p>
   <p>Date: {{ attributes.createdAt }}</p>
+  <ul v-for="note in attributes.notes" :key="note">
+    <li>{{ note }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -21,7 +24,10 @@
         amount: 5000.05,
         attributes: {
           createdAt: "02.02.2002",
-          notes: []
+          notes: [
+            "Account opened successfully.",
+            "Monthly maintenance fee applied."
+          ]
         }
       }
     },
