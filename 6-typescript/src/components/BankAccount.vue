f3d1c14806a2bd1305e6a44347f3e9759133f36b
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-29 11:07:00 +0200

    chore: add typed attributes object to data()

--- 6-typescript/src/components/BankAccount.vue
+++ 6-typescript/src/components/BankAccount.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>My name is {{ name }}, and I'm {{ age }} years old</h1>
+  <p>{{ attributes.height }}</p>
   <button @click="testChange('Marko', 29)">Test change</button>
 </template>
 
@@ -11,11 +12,16 @@
     name: "BankAccount",
     data(): {
       name: string,
-      age: number | null
+      age: number | null,
+      attributes: {height: number, weight: number}
     } {
       return {
         name: "Vica",
-        age: 28
+        age: 28,
+        attributes: {
+          height: 181.5,
+          weight: 100.08
+        }
       }
     },
     methods: {
