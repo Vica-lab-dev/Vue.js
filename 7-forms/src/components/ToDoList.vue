f064c3f52259658d97c54cc6d305d2a5deec1116
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-31 09:58:19 +0200

    feat: add task details to task creation

--- 7-forms/src/components/ToDoList.vue
+++ 7-forms/src/components/ToDoList.vue
@@ -2,28 +2,40 @@
   <h1>Hello World</h1>
 
   <form @submit.prevent="addTask">
-    <input v-model="taskTitle" type="text" placeholder="Enter a task title" required>
+    <input v-model="title" type="text" placeholder="Enter a task title" required>
+    <input v-model="description" type="text" placeholder="Enter a description" required>
+    <input v-model="dueDate" type="date" required>
     <button>Save</button>
   </form>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import TaskType from "@/Types/TaskType";
 
 export default defineComponent({
   name: "ToDoList",
   data(): {
-    taskTitle: string
-    tasks: Array<string>
+    title: string
+    description: string
+    dueDate: string
+    tasks: TaskType[]
   } {
     return {
-      taskTitle: "",
+      title: "",
+      description: "",
+      dueDate: "",
       tasks: []
     }
   },
   methods: {
     addTask() {
-      this.tasks.push(this.taskTitle);
+      const tempTask = {
+        title: this.title,
+        description: this.description,
+        dueDate: this.dueDate
+      }
+      this.tasks.push(tempTask);
     }
   }
 });
