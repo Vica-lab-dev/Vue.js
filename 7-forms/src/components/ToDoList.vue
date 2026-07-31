6bd14dddab5760623d7063b5f210548c03f0c15a
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-07-31 09:33:21 +0200

    feat: add task title form and data property

--- 7-forms/src/components/ToDoList.vue
+++ 7-forms/src/components/ToDoList.vue
@@ -1,11 +1,23 @@
 <template>
   <h1>Hello World</h1>
+
+  <form action="">
+    <input v-model="taskTitle" type="text" placeholder="Enter a task title" required>
+    <button>Save</button>
+  </form>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 
 export default defineComponent({
-  name: "ToDoList"
-})
+  name: "ToDoList",
+  data(): {
+    taskTitle: string
+  } {
+    return {
+      taskTitle: ""
+    }
+  }
+});
 </script>
