ab354e5d5f394f915a3f62439dd2efff9a510c9d
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-08-04 17:27:39 +0200

    feat: add max validation rule

--- 7-forms/src/main.ts
+++ 7-forms/src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { defineRule } from "vee-validate";
-import { required, min } from "@vee-validate/rules"
+import { required, min, max } from "@vee-validate/rules"
 
 defineRule("required", required)
 defineRule("min", min)
+defineRule("max", max)
 
 createApp(App).mount('#app')
