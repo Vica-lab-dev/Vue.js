82cb72c06e7937c15ca43b8f2d4b238b98fd56f5
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-08-22 07:08:33 +0200

    feat: add allTask.vue

---
+++ 7-forms/src/components/tasks/allTasks.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import {BoardType} from "@/Types/boards/BoardType";
+import TaskType from "@/Types/TaskType";
+import SingleTask from "@/components/tasks/singleTask.vue";
+
+const props = defineProps<{
+  groupedTasks: Record<BoardType, TaskType[]>
+}>();
+</script>
+
+<template>
+  <div class="allTasks">
+    <div
+        class="task-group"
+        v-for="(tasksInGroup, index) in groupedTasks"
+        :key="index"
+    >
+      <h2 class="group-title">{{ index }}</h2>
+
+      <div class="task-list">
+        <SingleTask v-for="task in tasksInGroup"
+        :key="task.id" :task="task"></SingleTask>
+      </div>
+    </div>
+  </div>
+</template>
