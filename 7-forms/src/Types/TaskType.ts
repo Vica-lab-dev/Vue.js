d0d2f153c09c20529fd56bb29847535a241861e9
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-08-22 05:18:15 +0200

    feat: add BoardType and import it into TaskType

--- 7-forms/src/Types/TaskType.ts
+++ 7-forms/src/Types/TaskType.ts
@@ -1,8 +1,10 @@
+import {BoardType} from "@/Types/boards/BoardType";
+
 export default interface TaskType {
     id: string
     title: string
     description: string
     dueDate: string,
     priority: "urgent" | "highpriority" | "mediumpriority" | "lowpriority" | null,
-    board: "todo" | "doing" | "done" | null,
+    board: BoardType,
 }
